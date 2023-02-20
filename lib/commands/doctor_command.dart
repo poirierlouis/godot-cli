c8b5bb9f0a124d93bb1608698b05d01ae18bd761
Louis Poirier <contact@louispoirier.io>
2023-02-20 14:35:55 +0100

    docs: add documentation on DoctorCommand.

--- lib/commands/doctor_command.dart
+++ lib/commands/doctor_command.dart
@@ -4,6 +4,7 @@ import 'package:gd/services/app_service.dart';
 import 'package:gd/services/detect_service.dart';
 import 'package:gd/ui/doctor_ui.dart';
 
+/// Entry of an executable program to detect and analyze.
 class DoctorProgram {
   DoctorProgram({
     required this.programTitle,
@@ -13,13 +14,23 @@ class DoctorProgram {
     this.delegate,
   });
 
+  /// Name of program used in UI.
   final String programTitle;
+
+  /// Service implementation to detect this program.
   final DetectService service;
+
+  /// Url link where to look for to install this program (OS dependent).
   final String installUrl;
+
+  /// Optional list of additional messages to show when program is not installed.
   final List<String>? installHelp;
+
+  /// Optional list of data to delegate additional detection / analysis with [DetectService] implementation.
   final List<dynamic>? delegate;
 }
 
+/// Abstract class with common features to execute 'doctor' command.
 abstract class DoctorCommand extends Command {
   @override
   final name = "doctor";
@@ -31,5 +42,6 @@ abstract class DoctorCommand extends Command {
   final AppService app = AppService.instance;
   final DoctorUi ui = DoctorUi();
 
+  /// List of programs to be detected and analyzed.
   List<DoctorProgram> get programs;
 }
