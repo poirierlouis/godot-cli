5a292c81927601a7357cba07812e8b9e20afd87d
Louis Poirier <contact@louispoirier.io>
2023-02-26 11:54:31 +0100

    fix: set default version number in Config to current application's version.
    docs: add missing documentation on Config.

--- lib/config.dart
+++ lib/config.dart
@@ -1,11 +1,20 @@
+import 'package:gd/commands/doctor_command.dart';
+import 'package:gd/platform_version.dart';
 import 'package:gd/sem_ver.dart';
 
 class Config {
   Config({required this.version, required this.programs, required this.isFirstRun, required this.issues});
 
+  /// Current version of [this] application.
   final SemVer version;
+
+  /// Path where to locate program to execute, e.g. `program: path`.
   final Map<String, String> programs;
+
+  /// Returns true when [this] application is run for the first time.
   bool isFirstRun;
+
+  /// Number of issues detected since the last run of [DoctorCommand].
   int issues;
 
   Map<String, dynamic> toJson() {
@@ -23,7 +32,7 @@ class Config {
 
   factory Config.fromJson(Map<String, dynamic> json) {
     return Config(
-      version: SemVer.parse(json["version"] ?? "1.0.0"),
+      version: SemVer.parse(json["version"] ?? packageVersion),
       programs: Map<String, String>.from(json["programs"] ?? {}),
       isFirstRun: json["is_first_run"] ?? true,
       issues: json["issues"] ?? -1,
