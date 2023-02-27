15e640bf9f7b9964f48e025aa9db00751c560a2a
Louis Poirier <contact@louispoirier.io>
2023-02-27 13:23:47 +0100

    refactor: remove unused version number in Config.

--- lib/config.dart
+++ lib/config.dart
@@ -1,12 +1,11 @@
 import 'package:gd/commands/doctor_command.dart';
-import 'package:gd/platform_version.dart';
-import 'package:gd/sem_ver.dart';
 
 class Config {
-  Config({required this.version, required this.programs, required this.isFirstRun, required this.issues});
-
-  /// Current version of [this] application.
-  final SemVer version;
+  Config({
+    required this.programs,
+    required this.isFirstRun,
+    required this.issues,
+  });
 
   /// Path where to locate program to execute, e.g. `program: path`.
   final Map<String, String> programs;
@@ -19,7 +18,6 @@ class Config {
 
   Map<String, dynamic> toJson() {
     return {
-      "version": "$version",
       "programs": programs,
       "is_first_run": isFirstRun,
       "issues": issues,
@@ -28,7 +26,6 @@ class Config {
 
   factory Config.empty() {
     return Config(
-      version: SemVer.parse(packageVersion),
       programs: {},
       isFirstRun: true,
       issues: -1,
@@ -37,7 +34,6 @@ class Config {
 
   factory Config.fromJson(Map<String, dynamic> json) {
     return Config(
-      version: SemVer.parse(json["version"] ?? packageVersion),
       programs: Map<String, String>.from(json["programs"] ?? {}),
       isFirstRun: json["is_first_run"] ?? true,
       issues: json["issues"] ?? -1,
