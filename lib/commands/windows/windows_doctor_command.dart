95bab73999675460a4cfa7764f42a609979a4bf3
Louis Poirier <contact@louispoirier.io>
2023-03-06 12:35:03 +0100

    refactor: move reusable logic of command 'doctor' from Windows implementation to base implementation.

--- lib/commands/windows/windows_doctor_command.dart
+++ lib/commands/windows/windows_doctor_command.dart
@@ -1,12 +1,6 @@
-import 'dart:io';
-
 import 'package:gd/commands/doctor_command.dart';
-import 'package:gd/extensions/string_utils.dart';
-import 'package:gd/platform_flavor.dart';
 import 'package:gd/sem_ver.dart';
-import 'package:gd/services/detect_service.dart';
 import 'package:gd/services/git_service.dart';
-import 'package:gd/services/program_service.dart';
 import 'package:gd/services/python_service.dart';
 import 'package:gd/services/visual_studio_service.dart';
 
@@ -35,94 +29,4 @@ class WindowsDoctorCommand extends DoctorCommand {
       delegate: [],
     ),
   ];
-
-  @override
-  Future<void> run() async {
-    if (argResults == null) {
-      return;
-    }
-    final isFirstRun = app.config.isFirstRun;
-
-    if (app.config.isFirstRun) {
-      await app.setFirstRun();
-    }
-    int issues = 0;
-
-    for (final program in programs) {
-      if (!await detectProgram(program)) {
-        issues++;
-      }
-    }
-    if (!await detectHomeEnvironment(isFirstRun)) {
-      issues++;
-    }
-    await app.setIssues(issues);
-    print("");
-    if (issues != 0) {
-      print("${"!".yellow} Doctor found $issues issue${issues > 1 ? "s" : ""}.");
-    } else {
-      print("${"√".green} Doctor found no issue.");
-    }
-  }
-
-  /// Detects [program] on user's system.
-  ///
-  /// Returns true when [program] is detected and well-configured.
-  ///
-  /// Prints logs depending on detection result.
-  Future<bool> detectProgram(final DoctorProgram program) async {
-    final DetectService service = program.service;
-
-    ui.withProgram(program);
-    try {
-      await service.isInstalled();
-
-      if (!service.hasRequiredVersion()) {
-        ui.printRequiredVersion(service.version!, service.requiredVersion!);
-        final path = app.getProgram(program.programTitle);
-
-        if (path != null) {
-          ui.printFixPath(path);
-        }
-        return false;
-      }
-      ui.printDetected(service.version!);
-      if (program.delegate != null && !await service.delegate(program.delegate!)) {
-        return false;
-      }
-      return true;
-    } on ProgramNotFound {
-      ui.printNotFound();
-      ui.printInstallHelp();
-      return false;
-    } on ProgramFailure {
-      ui.printMissingSemVer();
-    }
-    return false;
-  }
-
-  /// Detects GODOT_CLI_HOME environment variable on user's system.
-  ///
-  /// Returns true when environment variable is detected and well-configured.
-  ///
-  /// Prints logs depending on detection result.
-  Future<bool> detectHomeEnvironment(final bool isFirstRun) async {
-    final appDataPath = app.appData.path;
-
-    if (isFirstRun) {
-      ui.printHomeEnvDisclaimer(appDataPath);
-      return false;
-    }
-    final home = Platform.environment[kHome];
-
-    if (home == null) {
-      ui.printHomeEnvNotFound(appDataPath);
-      return false;
-    } else if (home != appDataPath) {
-      ui.printWrongHomeEnv(appDataPath);
-      return false;
-    }
-    ui.printHomeEnvDetected(appDataPath);
-    return true;
-  }
 }
