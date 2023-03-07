e051664afd8d377f177ecbbebc7de3cc962ab70f
Louis Poirier <contact@louispoirier.io>
2023-03-07 11:07:09 +0100

    fix: use custom path to GCC when provided with command 'config' on Linux.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- command 'doctor' on Linux use custom path to GCC when provided with command 'config'. 
 
 ------------------------
 
--- lib/services/gcc_service.dart
+++ lib/services/gcc_service.dart
@@ -1,5 +1,6 @@
 import 'package:gd/sem_ver.dart';
 import 'package:gd/services/detect_service.dart';
+import 'package:path/path.dart' as p;
 
 class GCCService extends DetectService {
   static final GCCService instance = GCCService._();
@@ -7,7 +8,14 @@ class GCCService extends DetectService {
   GCCService._();
 
   @override
-  final String executable = "gcc";
+  String get executable {
+    final path = app.getProgram("GCC");
+
+    if (path == null) {
+      return "gcc";
+    }
+    return p.join(path, "gcc");
+  }
 
   @override
   final SemVer? requiredVersion = SemVer(7);
